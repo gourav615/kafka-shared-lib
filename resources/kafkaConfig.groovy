1386cf5a1713688fd151872108841fdc6a6de07f
gourav615 <ggouravsharma674@gmail.com>
2026-01-18 23:53:53 +0530

    Add Kafka configuration for production environment

--- resources/kafkaConfig.groovy
+++ resources/kafkaConfig.groovy
@@ -1,1 +1,7 @@
-
+return [
+    SLACK_CHANNEL_NAME  : "build-status",
+    ENVIRONMENT         : "prod",
+    CODE_BASE_PATH      : "env/prod",
+    ACTION_MESSAGE      : "Kafka deployment started",
+    KEEP_APPROVAL_STAGE : true
+]
